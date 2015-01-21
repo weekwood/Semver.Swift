a7e3573ad3aa18f1378ad8fea2b2ca3e3c13bc5b
di wu <di.wu@me.com>
2015-01-21 22:59:22 +0800

    add valid
    add clean

--- Semver/Semver/Semver.swift
+++ Semver/Semver/Semver.swift
@@ -8,22 +8,111 @@
 
 import Foundation
 
+struct Regex {
+    var pattern: String {
+        didSet {
+            updateRegex()
+        }
+    }
+    var expressionOptions: NSRegularExpressionOptions {
+        didSet {
+            updateRegex()
+        }
+    }
+    var matchingOptions: NSMatchingOptions
+    
+    var regex: NSRegularExpression?
+    
+    init(pattern: String, expressionOptions: NSRegularExpressionOptions, matchingOptions: NSMatchingOptions) {
+        self.pattern = pattern
+        self.expressionOptions = expressionOptions
+        self.matchingOptions = matchingOptions
+        updateRegex()
+    }
+    
+    init(pattern: String) {
+        self.pattern = pattern
+        expressionOptions = NSRegularExpressionOptions(0)
+        matchingOptions = NSMatchingOptions(0)
+        updateRegex()
+    }
+    
+    mutating func updateRegex() {
+        regex = NSRegularExpression(pattern: pattern, options: expressionOptions, error: nil)
+    }
+}
+
+extension String {
+    func matchRegex(pattern: Regex) -> Bool {
+        let range: NSRange = NSMakeRange(0, countElements(self))
+        if pattern.regex != nil {
+            let matches: [AnyObject] = pattern.regex!.matchesInString(self, options: pattern.matchingOptions, range: range)
+            return matches.count > 0
+        }
+        return false
+    }
+    
+    func match(patternString: String) -> Bool {
+        return self.matchRegex(Regex(pattern: patternString))
+    }
+    
+    func replaceRegex(pattern: Regex, template: String) -> String {
+        if self.matchRegex(pattern) {
+            let range: NSRange = NSMakeRange(0, countElements(self))
+            if pattern.regex != nil {
+                return pattern.regex!.stringByReplacingMatchesInString(self, options: pattern.matchingOptions, range: range, withTemplate: template)
+            }
+        }
+        return self
+    }
+    
+    func replace(pattern: String, template: String) -> String {
+        return self.replaceRegex(Regex(pattern: pattern), template: template)
+    }
+}
+
+
 public class Semver {
+    
+    let SemVerRegexp = "\\A(\\d+\\.\\d+\\.\\d+)(-([0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*))?(\\+([0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*))?\\Z"
     
     let major: String = ""
     let minor: String = ""
     let patch: String = ""
+    let pre: String = ""
+    let build: String = ""
+    var versionStr: String = ""
+    
+    required public init(){
+    }
+    
+    convenience init(version: String!){
+        self.init()
+        self.versionStr = version
+    }
     
     func diff(version1: String, version2: String) -> Bool{
         return false
     }
     
-    public func valid(version: String) -> Bool{
+    public class func valid(version: String) -> Bool{
+        return Semver(version: version).valid()
+    }
+    
+    public func valid() -> Bool{
+        if let match = self.versionStr.rangeOfString(SemVerRegexp, options: .RegularExpressionSearch){
+            return true
+        }
         return false
     }
     
-    public func clean(version: String) -> String{
-        return ""
+    public class func clean(version: String) -> String{
+        return Semver(version: version).clean()
+    }
+    
+    public func clean() -> String{
+        versionStr = versionStr.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        return versionStr.replace("^[=v]+", template: "")
     }
     
     public func gt(version1: String, version2: String) -> Bool{
@@ -43,9 +132,7 @@ public class Semver {
     }
     
     func parse(version: String) -> Semver{
-        return Semver()
+        return Semver(version: version)
     }
-    
-
     
 }
