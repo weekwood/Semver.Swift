8e1bc93f7a7afcc8ab3114e2b40a245537ef034f
Tsuharesu Luciel <tsuharesu01@gmail.com>
2015-03-25 14:27:06 -0300

    Creating workspace and putting Framework target on Semver

--- SemverSwiftTests/SemverSwiftTests.swift
+++ SemverSwiftTests/SemverSwiftTests.swift
@@ -1,31 +1,33 @@
 //
-//  SemverTests.swift
-//  SemverTests
+//  SemverSwiftTests.swift
+//  SemverSwiftTests
 //
-//  Created by di wu on 2/17/15.
-//  Copyright (c) 2015 di wu. All rights reserved.
+//  Created by Thales Lima on 3/25/15.
+//  Copyright (c) 2015 Semver. All rights reserved.
 //
 
 import UIKit
 import XCTest
-import Semver
-class SemverTests: XCTestCase {
+
+class SemverSwiftTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testVaild() {
+    func testValid() {
         XCTAssertFalse(Semver.valid("a.b.c"), "pass")
         XCTAssertTrue(Semver.valid("1.3.4-pre.1+ver.1"), "pass")
         XCTAssertTrue(Semver.valid("1.3.4+ver.1"), "pass")
         XCTAssertTrue(Semver.valid("1.3.4-pre.1"), "pass")
     }
-
+    
     func testClean() {
         XCTAssertEqual("1.2.3", Semver.clean("   =v1.2.3     "), "pass")
     }
@@ -37,4 +39,5 @@ class SemverTests: XCTestCase {
     func testGt() {
         XCTAssertFalse(Semver.gt("1.2.3", version2: "9.8.1"), "pass")
     }
+    
 }
