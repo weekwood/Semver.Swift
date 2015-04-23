1a28ff8dc260bc5c915d90b84fd76fe455d5f2f7
Tsuharesu Luciel <tsuharesu01@gmail.com>
2015-04-23 11:15:35 -0300

    updating for Swift 1.2

--- SemverSwift/Semver.swift
+++ SemverSwift/Semver.swift
@@ -44,7 +44,7 @@ struct Regex {
 
 extension String {
     func matchRegex(pattern: Regex) -> Bool {
-        let range: NSRange = NSMakeRange(0, countElements(self))
+        let range: NSRange = NSMakeRange(0, count(self))
         if pattern.regex != nil {
             let matches: [AnyObject] = pattern.regex!.matchesInString(self, options: pattern.matchingOptions, range: range)
             return matches.count > 0
@@ -58,7 +58,7 @@ extension String {
     
     func replaceRegex(pattern: Regex, template: String) -> String {
         if self.matchRegex(pattern) {
-            let range: NSRange = NSMakeRange(0, countElements(self))
+            let range: NSRange = NSMakeRange(0, count(self))
             if pattern.regex != nil {
                 return pattern.regex!.stringByReplacingMatchesInString(self, options: pattern.matchingOptions, range: range, withTemplate: template)
             }
